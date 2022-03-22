26771563240622a9b4674c3cc975e2ee85d8592f
Jared_P <jaredpisell@gmail.com>
2022-03-21 22:24:18 -0700

    Added Lesson 2

---
+++ Node Fundamentals/Node101/lesson2.js
@@ -0,0 +1,14 @@
+//Modules 
+const http = require('http');
+
+//SERVER
+//The http object from node allows the server to instantiate and be a listener. 
+/*--------------------------------------------------------- */
+const server = http.createServer((request, response) => {
+    console.log(request);
+    response.end("Hello from the server!");
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Server listening to requests on -> port 8000");
+})
